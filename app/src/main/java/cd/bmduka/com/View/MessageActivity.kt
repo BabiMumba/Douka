966c1da993f5a5964d8c3d77f6b809a512d4d28b
babimumba <babimumba243@gmail.com>
2024-06-02 22:48:26 +0200

    add new model view

--- app/src/main/java/cd/bmduka/com/View/MessageActivity.kt
+++ app/src/main/java/cd/bmduka/com/View/MessageActivity.kt
@@ -3,48 +3,52 @@ package cd.bmduka.com.View
 import android.content.Context
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import cd.babi.chatal.models.Message
 import cd.bmduka.com.Adapter.MessageAdapter
-import cd.bmduka.com.R
 import cd.bmduka.com.Utils.Utils
+import cd.bmduka.com.ViewModel.MainViewModel
 import cd.bmduka.com.databinding.ActivityMessageBinding
 
+
 class MessageActivity : AppCompatActivity() {
-    var liste_message= ArrayList<Message>()
+   var liste_message= ArrayList<Message>()
     lateinit var binding:ActivityMessageBinding
+    private var viewModel= MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMessageBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        InitMessage()
+        InitMessages()
+
         binding.btnSend.setOnClickListener {
             val message = binding.message.text.toString()
             if (message.isNotEmpty()){
-                liste_message.add(Message(message,true))
+                viewModel.addMessage(message)
                 binding.message.setText("")
                 val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 inputManager.hideSoftInputFromWindow(binding.message.windowToken,0)
+            //    binding.recycleMessage.scrollToPosition(liste_message.size-1)
             }else{
                 Utils.showToast(this,"votre message")
             }
         }
-        binding.recycleMessage.adapter!!.notifyDataSetChanged()
+
 
 
     }
 
-    fun InitMessage(){
-        liste_message.add(Message("bonjour",false))
-        liste_message.add(Message("comment tu va",true))
-        liste_message.add(Message("bien et toi",false))
-        binding.recycleMessage.apply {
-            adapter = MessageAdapter(liste_message)
 
+    fun InitMessages(){
+        viewModel.messages.observe(this){items->
+            for (item in items){
+                liste_message.add(item)
+            }
+            binding.recycleMessage.apply {
+                adapter = MessageAdapter(liste_message)
+            }
         }
-        binding.recycleMessage.adapter!!.notifyDataSetChanged()
+        viewModel.FetchMessage()
     }
 }
--- app/src/main/java/cd/bmduka/com/ViewModel/MainViewModel.kt
+++ app/src/main/java/cd/bmduka/com/ViewModel/MainViewModel.kt
@@ -3,6 +3,7 @@ package cd.bmduka.com.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cd.babi.chatal.models.Message
 import cd.bmduka.com.Model.SliderModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -13,13 +14,17 @@ class MainViewModel():ViewModel() {
     private val firebaseDatabase = FirebaseDatabase.getInstance()
     private val _banner = MutableLiveData<List<SliderModel>>()
     val banners: LiveData<List<SliderModel>> = _banner
+    val liste_message= MutableLiveData<ArrayList<Message>>()
+    val messages: MutableLiveData<ArrayList<Message>> = liste_message
+
+    val liste = ArrayList<Message>()
+
 
     fun loadBanners(){
         val ref = firebaseDatabase.getReference("Banner")
         ref.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val listes = mutableListOf<SliderModel>()
-
                 for (snap in snapshot.children){
                     val banner = snap.getValue(SliderModel::class.java)
                     if (banner != null){
@@ -35,6 +40,16 @@ class MainViewModel():ViewModel() {
                 _banner.value = null
             }
         })
+    }
+
+    fun FetchMessage(){
+        liste.add(Message("bonjour",false))
+        liste.add(Message("comment tu va",true))
+        liste.add(Message("bien et toi",false))
+        liste_message.value = liste
+    }
+    fun addMessage(msg:String){
+        liste.add(Message(msg,true))
     }
 
 }
